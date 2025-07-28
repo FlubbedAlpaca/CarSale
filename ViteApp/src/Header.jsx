f7752b8be01a948305c06bd03da331a9add9db4c
FlubbedAlpaca <82659352+FlubbedAlpaca@users.noreply.github.com>
2025-07-27 20:52:22 -0400

    Add files via upload

---
+++ ViteApp/src/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>SettleBot</h1>
+      <p>Your partner in conflict resolution</p>
+    </header>
+  );
+}
+
+export default Header;
+
