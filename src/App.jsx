fbeff95c930bb0a6ce7354f47e82bd767de601ce
FlubbedAlpaca <82659352+FlubbedAlpaca@users.noreply.github.com>
2025-07-27 21:02:10 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// File: src/App.jsx
+import Gallery from './Gallery';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">Custom Car Photo Gallery</h1>
+      <Gallery />
+    </div>
+  );
+}
